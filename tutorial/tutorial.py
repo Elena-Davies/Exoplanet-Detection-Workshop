b8b6f59a1b021b3c97ad38812d49cb0df3328904
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2023-10-29 23:05:30 +0000

    import time

--- tutorial/tutorial.py
+++ tutorial/tutorial.py
@@ -69,3 +69,7 @@ left_column.button('Press me!')
 with right_column:
     chosen = st.radio('Sorting hat',("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
     st.write(f"You are in {chosen} house!")
+
+# showing progress
+import time
+
