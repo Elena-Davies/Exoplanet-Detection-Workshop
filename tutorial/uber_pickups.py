7356c82940800cefe25defd38637c813481e47ae
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2023-10-29 22:29:29 +0000

    gather example data

--- tutorial/uber_pickups.py
+++ tutorial/uber_pickups.py
@@ -7,3 +7,16 @@ st.title('Uber pickups in NYC')
 
 # run streamlit
 st.run https://github.com/Elena-Davies/Exoplanet-Detection-Workshop/blob/tutorial/tutorial/uber_pickups.py
+
+# Gather example data
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
