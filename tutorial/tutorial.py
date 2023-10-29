84beeeceeabce33a6afb473f616bfe666e903012
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2023-10-29 23:04:19 +0000

    Update tutorial.py

--- tutorial/tutorial.py
+++ tutorial/tutorial.py
@@ -6,6 +6,9 @@ df = pd.DataFrame({'first column': [1,2,3,4],'second column':[10,20,30,40]})
 
 df
 
+# run streamlit
+st.run https://github.com/Elena-Davies/Exoplanet-Detection-Workshop/blob/tutorial/tutorial/tutorial.py
+
 # Write a data frame
 
 st.write("Here's our first attempt at using data to create a table:")
@@ -24,4 +27,36 @@ dataframe3 = pd.DataFrame(np.random.randn(10,20),columns=('col %d' % i for i in 
 st.table(dataframe3)
 
 # Drawing a line chart
+chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
 
+st.line_chart(chart_data)
+
+# plotting a map
+map_data = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],columns=['lat', 'lon'])
+
+st.map(map_data)
+
+# adding widgets
+x = st.slider('x')
+st.write(x, 'squared is', x * x)
+
+st.text_input("Your name", key="name")
+
+# using checkboxes to show/hide data
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
+
+    chart_data
+
+# using a selectbox for options
+df = pd.DataFrame({'first column': [1, 2, 3, 4],'second column': [10, 20, 30, 40]})
+
+option = st.selectbox('Which number do you like best?',df['first column'])
+
+'You selected: ', option
+
+# Adding a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox('How would you like to be contacted?',('Email', 'Home phone', 'Mobile phone'))
+
+# Adding a slider to the sidebar:
+add_slider = st.sidebar.slider('Select a range of values',0.0, 100.0, (25.0, 75.0))
