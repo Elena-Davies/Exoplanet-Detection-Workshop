06703c28bfd1d1165be2534cfb8793ddbe22babb
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2023-10-29 23:06:05 +0000

    start long calculation

--- tutorial/tutorial.py
+++ tutorial/tutorial.py
@@ -73,3 +73,12 @@ with right_column:
 # showing progress
 import time
 
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
