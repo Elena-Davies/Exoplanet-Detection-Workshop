7a8e2402a57ceaa5aca9e5a25fc6c1339cc82674
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2023-10-30 07:31:28 +0000

    Update tutorial.py

--- tutorial/tutorial.py
+++ tutorial/tutorial.py
@@ -24,7 +24,7 @@ import numpy as np
 dataframe = np.random.randn(10,20)
 st.dataframe(dataframe)
 
-dataframe2 = pd.DataFrame(np.random.randn(10,20),columns=('col %d' % i for i in range(20)))
+dataframe = pd.DataFrame(np.random.randn(10,20),columns=('col %d' % i for i in range(20)))
 
 st.dataframe(dataframe.style.highlight_max(axis=0))
 
