81b9d60d7712a284e6edbeb0108c9e7fffaf503a
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2023-11-05 21:57:40 +0000

    Update week5practice.py

--- tutorial/week5practice.py
+++ tutorial/week5practice.py
@@ -4,12 +4,20 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-# Use Agg
+# Use Agg backend to be thread safe
 mpl.use("agg")
 
 # adding pages
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
+
+# Need to lock plots to be more thread-safe
+from matplotlib.backends.backend_agg import RendererAgg
+lock = RendererAgg.lock
+
+# Title the app
+apptitle = 'Week 5 Practice'
+st.title(apptitle)
 
 st.write("Here's a sin wave")
 
@@ -19,15 +27,15 @@ time = np.arange(0, 10, 0.1);
 # Amplitude of the sine wave is sine of a variable like time
 ampltiude = np.sin(time)
 # Plot a sine wave using time and amplitude obtained for the sine wave
-plt.plot(time, ampltiude)
+sinewave = plt.plot(time, ampltiude)
 # Give a title for the sine wave plot
 plt.title('Sine wave')
 # Give x axis label for the sine wave plot
 plt.xlabel('Time (s)')
 # Give y axis label for the sine wave plot
-plt.ylabel('Amplitude = sin(time) (m)')
+plt.ylabel('Ampltiude (m)')
 # Make the plot look presentable
 plt.grid(True, which='both')
 plt.axhline(y=0, color='k')
 #Display the sine wave
-plt.show()
+st.pyplot(sinewave)
