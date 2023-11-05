713b7c1a19b61365ab20bc0de45b5422371230fc
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2023-11-05 22:41:24 +0000

    added descriptions

--- tutorial/week5practice.py
+++ tutorial/week5practice.py
@@ -13,19 +13,24 @@ st.sidebar.markdown("# Main page 🎈")
 apptitle = 'Week 5 Practice'
 st.title(apptitle)
 
+# Add text
 st.write("Here's how a sine wave changes with time")
 
+# Add progress bar in sidebar
 progress_bar = st.sidebar.progress(0)
 status_text = st.sidebar.empty()
 
+# Set up empty chart
 chart = st.line_chart(np.zeros(shape=(1,1)))
+# x values
 x = np.arange(0, 100*np.pi, 0.1)
 
+# Animation 
 for i in range(1,101):
-    y = np.sin(x[i])
+    y = np.sin(x[i]) # y values
     status_text.text("%i%% Complete" % i)
-    chart.add_rows([y])
-    progress_bar.progress(i)
+    chart.add_rows([y]) # add each value
+    progress_bar.progress(i) # when progress bar is complete show "complete"
     time.sleep(0.05)
 
-progress_bar.empty()
+progress_bar.empty() # when progress bar is complete, progress bar disappears or becomes empty
