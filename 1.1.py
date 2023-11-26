ce9966d69249e13a528f6f4f9e9d7416a9f02dc1
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2023-11-26 22:38:10 +0000

    Update 1.1.py

--- 1.1.py
+++ 1.1.py
@@ -34,6 +34,7 @@ if section==1:
     # Add text
     st.write("Transits are used to detect exoplanets. Exoplanets are a word scientists use to refer to extrasolar planets, or planets that orbit stars outside of our solar system.")
     st.markdown("The transit method works by measuring the dimming effect that takes place when a planet passes in front of its host star. Scientists analyse this dimming effect in light curves which are graphs that show the light received over a period of time. Click on the light curve section to learn more about them!")
+    st.write("Stars emit as blackbodies, meaning they emit all wavelengths. A star's and exoplanet's atmosphere can be discovered by analysing the emission spectra of the star as seen from Earth. Click on the Spectra section to find out more!")
 
 if section==2:
     # spectra
