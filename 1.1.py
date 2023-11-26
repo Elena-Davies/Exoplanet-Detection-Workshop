567b5507094b78aa43b18709764a76794109172b
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2023-11-26 22:19:07 +0000

    backend

--- 1.1.py
+++ 1.1.py
@@ -4,3 +4,8 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import time
+
+# use the non-interactive Agg backend to be more thread safe
+#mpl.use("agg")
+from matplotlib.backends.backend_agg import RendererAgg
+_lock = RendererAgg.lock
